a3196226445d1b9f29731b661b9b34f84423d5b7
Vadim Kochergin <kochergin1990vadim@gmail.com>
2024-11-11 11:55:29 +0300

    PrimitivesTask1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int amount = 17867;
+        int rubMiles = 20;
+        int bonus = amount / rubMiles;
+        System.out.println("Бонусных миль получено: " + bonus);
+
+    }
+
+}
